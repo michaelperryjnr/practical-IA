975b8d69aa3a15a79f822a34616b3a078e1815b5
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-18 08:24:10 +0000

    Created patient model and schema

---
+++ models/patientModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoosse");
+
+const patientSchema = mongoose.Schema(
+  {
+    patient_Id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    other_names: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    residential_address: {
+      type: String,
+      required: true,
+    },
+    emergency_name: {
+      type: String,
+      required: true,
+    },
+    emergency_contact: {
+      type: String,
+      required: true,
+    },
+    relationshipt_with_emergency_contact: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Patient = mongoose.model("Patient", patientSchema);
+
+module.exports = Patient;
